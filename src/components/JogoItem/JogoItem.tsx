e5b460a40f58ccd5aaa359abfc2f748af7377274
FelipeCristo <felipe.cristo@yahoo.com.br>
2022-10-19 11:15:12 -0300

    estruturação de links dos cards e imagens

--- src/components/JogoItem/JogoItem.tsx
+++ src/components/JogoItem/JogoItem.tsx
@@ -15,18 +15,11 @@ function JogoItem() {
       <JogoItemName> nome do jogo</JogoItemName>
       <JogoItemPrice> anos</JogoItemPrice>
       <JogoItemDescription>
-        <Iframe
-          url="http://www.youtube.com/embed/xDMP3i36naA"
-          position="absolute"
-          width="100%"
-          
-          id="myId"
-          className="myClassname"
-          height="100%"
-          styles={{ height: "455px" }}
-        />
+        
+     
       </JogoItemDescription>
     </JogoItem1>
+    
   );
 }
 
